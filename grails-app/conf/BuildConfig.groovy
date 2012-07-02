9ec8169ab45ef4d7f5fee7dcd8efde1f3cba99b4
Burt Beckwith <beckwithb@vmware.com>
2012-07-02 13:20:11 -0400

    downgrade to release plugin v2.0.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,7 +15,7 @@ grails.project.dependency.resolution = {
 	dependencies {}
 
 	plugins {
-		build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+		build(':release:2.0.2', ':rest-client-builder:1.0.2') {
 			export = false
 		}
 	}
